96b5f1f3e2675e3d88595732528ef2e334c8abef
andreyromanov <andreygorogogo@gmail.com>
2020-01-10 22:20:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = '1005375475:AAHF25XkUu3SpqyjvrYyKxcdZQBqod2YMfE';
+
+const bot = new TelegramBot(TOKEN, {polling: true});
+
+bot.on('message', msg => {
+	bot.sendMessage(msg.chat.id, "hello traveller, ${msg.from.first_name}")
+})
