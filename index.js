6cb518309f59ab40422272aba25fe79512696752
andreyromanov <andreygorogogo@gmail.com>
2020-01-10 22:49:31 +0200

    Answer

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const TOKEN = '1005375475:AAHF25XkUu3SpqyjvrYyKxcdZQBqod2YMfE';
 const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
-	bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}`)
+	bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}!`)
 })
