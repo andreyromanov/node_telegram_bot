c690fc0fbd4fdfb1d88d5c877e3130f67330ce66
andreyromanov <andreygorogogo@gmail.com>
2020-01-13 16:31:50 +0200

    Connection to DB

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
+process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api');
+const mysql = require("mysql2");
 
-const TOKEN = '1005375475:AAHF25XkUu3SpqyjvrYyKxcdZQBqod2YMfE';
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "admin",
+  database: "bot_db",
+  password: "uatao"
+});
+
+
+
+
+const TOKEN = '1046316934:AAEp6Uy9xiKoI0KpYfRb3cQnX_a-VjfNSCA';
 
 const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.on('message', msg => {
 	bot.sendMessage(msg.chat.id, `Привет, ${msg.from.first_name}!`)
+
+	var sql = `INSERT INTO telegram_users (chat_id) VALUES (${msg.from.id}) ON DUPLICATE KEY UPDATE
+    chat_id = ${msg.from.id}`;
+	  connection.query(sql, function (err, result) {
+	    if (err) throw err;
+	    console.log("1 record inserted");
+	  });
+
 })
 
-
-
-
-
-const ViberBot = require('viber-bot').Bot;
-const BotEvents = require('viber-bot').Events;
-
-const bot = new ViberBot({
-	authToken: "4ae29d599827ddd3-b949f894682f6997-6b7417e64af5280",
-	name: "UaTaoBot",
-});
-
-// Perfect! Now here's the key part:
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-	// Echo's back the message to the client. Your bot logic should sit here.
-	response.send(message);
-});
-/*
-// Wasn't that easy? Let's create HTTPS server and set the webhook:
-const https = require('https');
-const port = process.env.PORT || 8080;
-
-// Viber will push messages sent to this URL. Web server should be internet-facing.
-const webhookUrl = process.env.WEBHOOK_URL;
-
-const httpsOptions = {
-	key: ...,
-	cert: ...,
-	ca: ...
-}; // Trusted SSL certification (not self-signed).
-https.createServer(httpsOptions, bot.middleware()).listen(port, () => bot.setWebhook(webhookUrl));*/
+//bot.sendMessage(391175023, `Завелся ботик`)
