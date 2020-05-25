e09760923c79e74d802c55f15ef6ea9a4e23b2bf
andreyromanov <andreygorogogo@gmail.com>
2020-05-25 16:09:17 +0300

    stages

--- routes/telegram.js
+++ routes/telegram.js
@@ -2,7 +2,59 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 
-const TelegramBot = require('node-telegram-bot-api');
+const Telegraf = require('telegraf')
+const Composer = require('telegraf/composer')
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Markup = require('telegraf/markup')
+const WizardScene = require('telegraf/scenes/wizard')
+
+const stepHandler = new Composer()
+stepHandler.action('next', (ctx) => {
+    ctx.reply('Step 2. Via inline button')
+    return ctx.wizard.next()
+})
+stepHandler.command('next', (ctx) => {
+    ctx.reply('Step 2. Via command')
+    return ctx.wizard.next()
+})
+stepHandler.use((ctx) => ctx.replyWithMarkdown('Press `Next` button or type /next'))
+
+const superWizard = new WizardScene('super-wizard',
+    (ctx) => {
+        ctx.reply('Step 1', Markup.inlineKeyboard([
+            Markup.urlButton('❤️', 'http://telegraf.js.org'),
+            Markup.callbackButton('➡️ Next', 'next')
+        ]).extra())
+        return ctx.wizard.next()
+    },
+    stepHandler,
+    (ctx) => {
+        ctx.reply('Step 3', Markup.inlineKeyboard([
+            Markup.urlButton('❤️', 'http://telegraf.js.org'),
+            Markup.callbackButton('➡️ Next', 'next')
+        ]).extra())
+        return ctx.wizard.next()
+    },
+    (ctx) => {
+        ctx.reply('Step 4')
+        return ctx.wizard.next()
+    },
+    (ctx) => {
+        ctx.reply('Done')
+        return ctx.scene.leave()
+    }
+)
+
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+const stage = new Stage([superWizard], { default: 'super-wizard' })
+
+bot.use(session())
+bot.use(stage.middleware())
+bot.launch()
+
+bot.startPolling()
+/*const TelegramBot = require('node-telegram-bot-api');
 
 const mysql = require("mysql2");
 
@@ -127,5 +179,5 @@ router.post('/', async (req,res) => {
 //send response to server
     res.send('posted')
 });
-
+*/
 module.exports = router;
